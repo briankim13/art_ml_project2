08794980c7b9a97721cf904c32c8d74d50f8df92
Brian Kim <wooshikk@andrew.cmu.edu>
2019-03-15 20:10:10 -0400

    change instance to match original

---
+++ Cityscapes_car2building/test_inst/change_inst2.py
@@ -0,0 +1,59 @@
+# change buildings to sky
+# change benz to road 
+from PIL import Image
+from os import listdir 
+# from os.path import isfile, join 
+
+# car: 26
+# building: 11
+# benz: 2
+# road: 7
+# sky: 23
+# tree: 21
+
+# car: 26       1,0,140
+# building: 11  70,70,70
+# benz: 2       0,0,0
+# road: 7       129,63,125
+# sky: 23       68~70,130,179~180
+# lawn: 22      150,250,152
+# sidewalk: 8   244,35,232
+# sidewalk?:6   81,0,81
+# pole: 17      153,153,153 ~158
+# other?: 4     0,0,0
+# sign: 20      221,221,0
+# tree: 21      105,142,36
+# ppl:  25      221,22,65
+# bicycle: 33   119,11,32  
+# :        9    250,170,160
+# guardrail:13  190,153,153  
+#           5   111,74,0
+#
+
+# Instance IDs 
+# cars: 101 
+# bicycle: 128 
+# people: 93
+
+imgs = [] 
+files = listdir() 
+for file in files:
+	if file.endswith(".png"):
+		imgs.append(file) 
+
+idx = 0 
+for im_name in imgs:
+	im = Image.open(im_name)
+	pix = im.load()
+
+	for i in range(im.size[0]):
+		for j in range(im.size[1]):
+			if pix[i,j] == 255: # to dark 
+				pix[i,j] = 0 
+			else:
+				pix[i,j] = 101 # to car instance
+
+	im.save('changed2/%s'%im_name) 
+	print("saved as %s"%im_name)
+	idx = idx + 1 
+
